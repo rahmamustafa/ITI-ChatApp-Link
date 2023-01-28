0f67e6c8dcd19ac8f5ebf6b67468281d2fad0260
Careem07 <kareemalaa473@gmail.com>
2023-01-29 00:12:34 +0200

    login fxml and controller

--- client/src/main/java/gov/iti/link/presentation/controllers/LoginController.java
+++ client/src/main/java/gov/iti/link/presentation/controllers/LoginController.java
@@ -1,25 +1,77 @@
 package gov.iti.link.presentation.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import gov.iti.link.business.DTOs.UserDTO;
 import gov.iti.link.business.services.UserService;
 import gov.iti.link.business.services.UserServiceImp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
-public class LoginController {
+public class LoginController implements Initializable{
+
     @FXML
-    private TextField tfPhone;
+    private Button btnLogin;
+
     @FXML
     private Button btnRegister;
 
-    public void onRegister(ActionEvent e){
-        System.out.println("register");
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Label lblErr;
+
+    @FXML
+    private Label lblLeft;
+
+    @FXML
+    private AnchorPane leftPane;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtPhone;
+
+    @FXML
+    void onLogin(ActionEvent event) {
+
         UserDTO user = new UserDTO();
-        user.setPhone(tfPhone.getText());
-        user.setName("zaki");
+        setUserData(user);
         UserService userService = new UserServiceImp();
-        userService.save(user);
+        if(userService.findByPhone(user.getPhone()) != null ){
+
+            //go to next page
+        }
+        else{
+            lblErr.setVisible(true);
+        }
+
     }
+
+    @FXML
+    void onSgnUp(ActionEvent event) {
+
+    }
+
+    void setUserData(UserDTO user){
+        user.setPhone(txtPhone.getText());
+        user.setPassword(txtPassword.getText());
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // TODO Auto-generated method stub
+        
+    }
+
 }
